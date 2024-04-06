5a1bde5e0a240a4cd03121b654b6115bff8b22d7
NaTi Jhon <yohannesnatnael00@gmail.com>
2024-04-06 23:15:13 +0300

    Accessning DOM DONE

--- 2. Accessnig the DOM/exercise/app.js
+++ 2. Accessnig the DOM/exercise/app.js
@@ -1,5 +1,12 @@
 // 1. Select element h4 which holds text of "Red" by using getElementsByTagName
+
+console.log(document.getElementsByTagName("h4"))
 // 2. Select div with the class of "green" which holds text (Green) by using getElementsByTagName
-// 3. Select div with the ID of blue which holds the text "Blue" by using getElementsByClassName
-// 4. Select div which has the class & Id of "yello" by using querySelector()
+console.log(document.getElementsByClassName("green"))
+// 3. Select div with the ID of blue which holds the text "Blue" by using getElementsByIdName
+console.log(document.getElementById("blue"))
+// 4. Select div which has the class & Id of "yellow" by using querySelector()
+
+console.log(document.querySelector("#yellow"))
 // 5. Select all the elements which has the class of "teal" by using querySelectorAll
+console.log(document.querySelectorAll(".teal"))
--- 2. Accessnig the DOM/live-coding/app.js
+++ 2. Accessnig the DOM/live-coding/app.js
@@ -1,9 +1,17 @@
 // ************ DOM SELECTORS ************
 // 1. getElementsByTagName
+console.log(document.getElementsByTagName("h1"))
+console.log(document.getElementsByTagName("h1").length)
 // 2. getElementById
+console.log(document.getElementById("main"))
 // 3. getElementsByClassName
+console.log(document.getElementsByClassName("cls"))
 // 4. querySelector
+// In this section if we want to select element by tag we write it inside the quotes , for id we use #idname and for classes .classname querySelector("here")
+console.log(document.querySelector("h1"))  
 // 5. querySelectorAll
-
+console.log(document.querySelectorAll("li").length)
 // ---------------------------------------
 // Storing data in variables
+let i = document.getElementsByClassName("cls")
+console.log(i)
