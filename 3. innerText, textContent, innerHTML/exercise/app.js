20b0fa858e332adbeec195c74d9f0901de3887ba
NaTi Jhon <yohannesnatnael00@gmail.com>
2024-04-08 21:00:28 +0300

    3. innerText, textContent, innerHTML DONE

--- 3. innerText, textContent, innerHTML/exercise/app.js
+++ 3. innerText, textContent, innerHTML/exercise/app.js
@@ -1,3 +1,6 @@
 // 1. Check the innerText of "first" element
+console.log(document.querySelector(".first").innerText)
 // 2. Check the textContent of "second" element
+console.log(document.querySelector(".second").textContent)
 // 3. Check the innerHTML of "third" element
+console.log(document.querySelector(".third").innerHTML)
--- 3. innerText, textContent, innerHTML/live coding/app.js
+++ 3. innerText, textContent, innerHTML/live coding/app.js
@@ -1,3 +1,17 @@
 // innerText
-// textContent
-// innerHTML
+console.log(document.querySelector("p").innerText)
+// textContent This will only give us the content without the HTML tags but if there is any js inside of it will give us all of them
+console.log(document.querySelector("p").textContent)
+// innerHTML This one will print every shit from the p
+console.log(document.querySelector("p").innerHTML)
+// And we can change the content of an HTML tad using innerText
+document.querySelector("h1").innerText = "What the fuck";
+setTimeout(() => {
+    document.querySelector("h1").innerHTML = "<mark> Hello </mark>";
+    setTimeout(() => {
+        document.querySelector("h1").innerHTML = "<mark> NaTi </mark>";
+        setTimeout(() => {
+            document.querySelector("h1").innerHTML = "<mark>Jhon</mark>";
+        }, 2000)
+    }, 2000) 
+}, 2000)
