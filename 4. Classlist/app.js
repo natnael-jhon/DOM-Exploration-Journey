da3e358fe2286dc9b6395c9132c4878bd02653ac
NaTi Jhon <yohannesnatnael00@gmail.com>
2024-04-08 21:59:47 +0300

    classList DONE

--- 4. Classlist/app.js
+++ 4. Classlist/app.js
@@ -1,4 +1,9 @@
 // classlist
+console.log(document.querySelector("h1").classList);
 // add()
+console.log(document.querySelector("h2").classList.add("styles"));
 // remove()
-// toggle()
+console.log(document.querySelector("h2").classList.remove("styles"));
+// toggle() toggle add class if the class is not present in the html if it is available on the html then it will remove it
+
+document.querySelector("h3").classList.toggle("styles")
