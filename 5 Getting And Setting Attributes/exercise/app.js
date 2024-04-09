a7c08dd9d9037494c03fe2c17e74d71eeaa2a858
Nathan Jhon <henrynatan88@gmail.com>
2024-04-09 21:17:18 +0300

    Getting and Setting Attributes

--- 5 Getting And Setting Attributes/exercise/app.js
+++ 5 Getting And Setting Attributes/exercise/app.js
@@ -1,4 +1,9 @@
 // 1. Select the anchor tag
+let i = document.querySelector("a")
 // 2. Use getAttribute(attrName) to check the attribute.
+console.log(i.getAttribute("href"))
 // 3. Select a-two & set "href" attribute
+document.querySelector(".a-two").innerText = "Telegram"
 // setAttribute(attrName, value)
+document.querySelector(".a-two").setAttribute("href", "https://t.me/nati0333")
+i.setAttribute("target", "_blank")
--- 5 Getting And Setting Attributes/live coding/app.js
+++ 5 Getting And Setting Attributes/live coding/app.js
@@ -1,5 +1,10 @@
 // href
+console.log(document.querySelector("a").href)
 // value
+console.log(document.querySelector("input").value = "hello")
 // type
+console.log(document.querySelector("input").type = "password")
 // getAttribute(attrName)
+console.log(document.querySelector("input").getAttribute("type"))
 // setAttribute(attrName, value)
+document.querySelector("input").setAttribute("placeholder", "Nigga")
