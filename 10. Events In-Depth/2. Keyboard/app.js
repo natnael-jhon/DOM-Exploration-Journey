de811f92ac09438bd4ab26a15b0c89b4ff3da8c3
NaTi Jhon <yohannesnatanel00@gamil.com>
2024-04-11 22:23:33 +0300

    Keyboard done

--- 10. Events In-Depth/2. Keyboard/app.js
+++ 10. Events In-Depth/2. Keyboard/app.js
@@ -1,2 +1,14 @@
 // https://www.w3schools.com/jsref/obj_keyboardevent.asp
 const input = document.querySelector("input");
+// input.addEventListener("keypress", () => {
+//     console.log("puusy")
+// });
+
+
+
+input.addEventListener("keypress", e =>{
+    // console.log(e.charCode) give nus some nums or keboard id
+    // console.log(e.code) gives us what is the key
+    // console.log(e.ctrlKey)
+    console.log(e.key)
+})
