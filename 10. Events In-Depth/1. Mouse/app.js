8f431832357f4e3f8eb6c6ca28cf7d01c6a95aa8
NaTi Jhon <yohannesnatanel00@gamil.com>
2024-04-11 19:56:19 +0300

    mouse event done

--- 10. Events In-Depth/1. Mouse/app.js
+++ 10. Events In-Depth/1. Mouse/app.js
@@ -21,3 +21,40 @@ let seven = document.querySelector(".mousemove");
 let eight = document.querySelector(".mouseout");
 let nine = document.querySelector(".mouseover");
 let ten = document.querySelector(".mouseup");
+
+
+one.addEventListener("click", () => {
+    one.classList.toggle("red")
+})
+
+two.addEventListener("contextmenu", () => {
+    two.classList.toggle("blue")
+})
+
+three.addEventListener("dblclick", () => {
+    three.classList.toggle("teal")
+})
+
+four.addEventListener("mousedown", () => {
+    four.classList.toggle("steelblue")
+})
+
+five.addEventListener("mouseenter", () => {
+    five.classList.toggle("orange")
+})
+
+six.addEventListener("mouseleave", () => {
+    six.classList.toggle("purple")
+})
+
+seven.addEventListener("mousemove", () => {
+    seven.classList.toggle("pink")
+})
+
+eight.addEventListener("mouseout", () => {
+    eight.classList.toggle("crimson")
+})
+
+nine.addEventListener("mouseover" ,() => {
+    nine.classList.toggle("chocolate")
+})
